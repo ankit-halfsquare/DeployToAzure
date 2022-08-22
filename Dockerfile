83527cb8c9537d4ced46e9619f791d78d40d07f9
ankit <ankit.kalal.halfsquare@gmail.com>
2022-08-22 10:42:31 +0530

    updated requirement.txt

--- Dockerfile
+++ Dockerfile
@@ -6,10 +6,4 @@ COPY ./requirements.txt /app/requirements.txt
 RUN pip install -r requirements.txt
 COPY . /app
 
-CMD ["python3", "manage.py", "runserver", "0.0.0.0:8000"]
 
-# RUN chmod +x /app/entrypoint.sh
-# RUN chmod +x /app/media
-
-
-# CMD ["/app/entrypoint.sh"]
--- demoProject/settings.py
+++ demoProject/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cxliskob&b)9yxg20)q$6elv6k!btg)v)sc!#ydohe9c%2o9tp'
+secret_key = os.getenv('SECRET_KEY')
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
